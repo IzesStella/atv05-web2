2a61f3e9ea08ef95f4b6514e5b563fe0db5fa9c1
Aluno Lab 03 <aluno.lab03@ifpe.edu>
2024-12-04 14:07:42 -0300

    atv5

---
+++ atividade5-web2-master/example-app/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PublisherController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Auth::routes();
+
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('categories', CategoryController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('publishers', PublisherController::class);
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
+Route::resource('books', BookController::class)->except(['create', 'store']);
